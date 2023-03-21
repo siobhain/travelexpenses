dbcb46af47803c6656ed3dee4afa5efd4cacfa16
siobhain <sobrien@live.ie>
2023-03-21 23:53:18 +0000

    Add main menu

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import gspread
 
 from google.oauth2.service_account import Credentials
 
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -19,11 +20,39 @@ SHEET = GSPREAD_CLIENT.open('ccd-travel-expenses')
 
 # emp = SHEET.worksheet('EngineSize')
 
+def get_main_menu():
+    """
+    This function asks user to chose and 
+    """
+
+    main_menu = ''' 
+        1) Enter 1 to Log a trip for travel expenses
+        2) Enter 2 to Generate a travel expenses report
+        3. Enter 3 to Approve travel expenses
+        4. Enter 4 to Exit
+        '''
+    print(main_menu)
+    user_input = ""
+    while user_input != '4':
+        try:
+            user_input = input("Enter 1, 2, 3 or 4 \n")
+            if user_input == "":
+                raise ValueError(" Input is blank ")
+            elif not user_input.isnumeric():
+                raise ValueError(" Numeric value only ")
+            elif int(user_input) > 0 and int(user_input) < 5:
+                pass
+            else:
+                raise ValueError(" Invalid numeric ")
+        except ValueError as e:        
+            print(f" {e}, Please try again. \n")
+
 
 def get_trip_data():
     """
     Ask user to enter 3 items to log the trip
     """
+
     print("You have chosen to enter trip details")
     print("Please enter trip in the format : Name, Destination, Date in dd/mm")
 
@@ -31,15 +60,21 @@ def get_trip_data():
         user_input = input(
             "Enter your trip details Example tarah, depo, 13/3 \n"
             )
-        trip_input = user_input.split(",")  # Into list using , as delimiter
-        if len(trip_input) != 3:
-            raise ValueError(
-                f"3 items required, You entered {len(trip_input)}"
-                )
-        else:
-            if validate_trip_data(trip_input):
-                print("Logging Trip.Put rest of code to update worksheet")
-                break
+        try:
+            trip_input = user_input.split(",")  # Into list using , as delimiter
+            if user_input == "":
+                raise ValueError("Input is blank, ")
+            elif len(trip_input) != 3:
+                raise ValueError(
+                    f"3 items required, You entered {len(trip_input)}"
+                    )
+            else:
+                if validate_trip_data(trip_input):
+                    print("Trip accepted, Logging details")
+                    break
+        except ValueError as e:
+            print(f"Invalid Data: {e}, Please try again. \n")
+
     return trip_input   
 
 
@@ -59,7 +94,7 @@ def validate_trip_data(trip_input):
     if verify_data(name_input, 'EngineSize'): 
         if verify_data(destination_input, 'Distance'):
             try:
-                trip_date = datetime.datetime(
+                trip_date = datetime.date(
                     2023, int(date_input_list[1]), int(date_input_list[0])
                     )
                 print(trip_date)
@@ -112,20 +147,21 @@ def create_trip_record(trip_input):
     trip_name = expand_data(trip_input[0], 'EngineSize')
     trip_destination = expand_data(trip_input[1], 'Distance')
     date_input_list = (trip_input[2]).split("/")
-    trip_date = datetime.datetime(
+    trip_date = datetime.date(
         2023, (int((date_input_list[1]))), (int((date_input_list[0])))
         ).strftime('%a %d %b %Y')
-    today = (datetime.datetime.now()).strftime('%a %d %b %Y')
+    today = (datetime.date.today()).strftime('%a %d %b %Y')
     print(today, trip_date, trip_name, trip_destination)
     
-    #trip_record = list("PENDING", today_date,trip_date,trip_name,trip_destination,trip_amount)
+    #trip_record = list("PENDING", today_date, trip_date,trip_name, trip_destination, trip_amount)
 
     rate_in_cent_per_km = get_milage_rate(trip_name)
     distance = get_distance_in_km(trip_destination)
-    amount = float(rate_in_cent_per_km) * int(distance)
-
+    amount = round(((float(rate_in_cent_per_km) * int(distance))/100), 2)
+    trip_record = list(("PENDING", today, trip_date, trip_name[0], trip_destination[0], amount))
     print(today, trip_date, trip_name, trip_destination, rate_in_cent_per_km, distance, amount)
-
+    print(trip_record)
+    return trip_record
 
 def get_milage_rate(name):
     """
@@ -145,7 +181,8 @@ def get_milage_rate(name):
     print("in milage rate")
     print(rate_column,row_number)
     return rate_column[row_number]
-    
+
+
 def get_distance_in_km(destination):
     """
     This function uses the destination send to it to #
@@ -162,38 +199,6 @@ def get_distance_in_km(destination):
     return distance_column[row_number]
 
 
-
-
-
-
-
-
-
-"""
-                position = name_column.index(full_name_string)
-                rate = rate_column[position]
-                position = destination_column.index(destination_string)
-                distance_column = distance_ws.col_values(2)
-                distance_column.pop(0)
-                print(distance_column)
-                distance = distance_column[position]
-                print(distance)
-                amount = round(float(rate) * int(distance))
-                print(amount)
-                print(
-                    datetime.datetime.now(), full_name_string,
-                    trip_date, amount
-                    )
-                travel_expenses_ws = SHEET.worksheet('TravelExpenses')
-                travel_expenses_ws.append_row(
-                    [(datetime.datetime.now()).strftime('%a %d %b %Y'),
-                     full_name_string, (trip_date.strftime('%a %d %b %Y')),
-                     amount]
-                    )
-
-"""
-
-
 def expand_data(data_input, worksheet):
     """
     This function takes the user input shorthand/substring
@@ -208,110 +213,39 @@ def expand_data(data_input, worksheet):
     print(f"Expand {data_input} to {full_data}")
     return full_data
 
+def submit_trip_record(trip_details):
+    """
+    This function updates the TravelExpenses worksheet with trip
+    entered by the user
+    """
 
-print("Welcome to CCD Travel Expenses - 2023 Log & Approvals")
-print("Press L to log trip(s), Press A to approve trip(s), Press H for help")
-# print("You have chosen to enter trip details, press # to return to 1st level")
-# print("Please enter trip in the format : Name, Destination, Date in dd/mm")
-# user_input = input("Enter your trip details Example Tarah, Depot, 23/3/23 \n")
+    travel_expenses_ws = SHEET.worksheet('TravelExpenses')
+    travel_expenses_ws.append_row(trip_details)
+    print(f"{trip_details} submitted to TravelExpenses worksheet")
 
-# validate trip data that user input
+def report_to_screen(worksheet_name):
+    """
+    This function will print worksheet details to screen
+    """
+    worksheet = SHEET.worksheet(worksheet_name)
+    data = worksheet.get_all_records(worksheet)
+    print(data)
 
-# trip_input = user_input.split(",")  # puts into a list using , as delimiter
 
-# validate correct number of items
-# need a while loop for continuous entry
+print("\nWelcome to CCD Travel Expenses - 2023 Log & Approvals")
 
-trip_data_input = get_trip_data()
+get_main_menu()
+
+#trip_data_input = get_trip_data()
 # All data entered is valid and verified, let expand it
-create_trip_record(trip_data_input)
-#trip_name = expand_data(trip_data_input[0], 'EngineSize')
+print("adding this record")
+
+#record = create_trip_record(trip_data_input)
+
+#print(record)
+#submit_trip_record(record)
+#trip_name = expand_data(trip_data_input[0], 'En
+# gineSize')
 #trip_destination = expand_data(trip_data_input[1],'Distance')
 # Create Trip record to be added to spreadsheet
 
-
-
-"""
-if len(trip_input) != 3:
-    raise ValueError(
-        f"3 items required : Name, Dest, Date You entered {len(trip_input)}"
-        )
-else:
-    name_input = trip_input[0]
-    destination_input = trip_input[1]
-    date_input = trip_input[2]
-    # verify name
-    enginesize_ws = SHEET.worksheet('EngineSize')
-    name_column = enginesize_ws.col_values(1)
-    # first column in EngineSize worksheet contains employee names
-    name_column.pop(0)
-    # remove element 0 from list which is the column title NAME
-
-    if name_input.lower() not in str(name_column).lower():
-        print(f"Invalid Name : {name_input}, Choose from {name_column}")
-        #input again
-    else:
-        full_name = [
-            name for name in name_column if name_input.lower() in name.lower()
-            ]
-        print(f"Name valid {full_name} - Next check Destinationa then date")
-        distance_ws = SHEET.worksheet('Distance')
-        destination_column = distance_ws.col_values(1)
-        destination_column.pop(0)
-        print(destination_column)
-        if destination_input.lower() not in str(destination_column).lower():
-            print(
-                f"Invalid Destination : {destination_input}, \
-                    Choose at least 3 letters from {destination_column}"
-                )
-        else:
-            destination = [
-                dest for dest in destination_column
-                if destination_input.lower() in dest.lower()
-            ]
-            print("Name & destination valid, now to check date")
-            date_input_list = date_input.split("/")
-            # puts each item input into a list using , as delimiter
-            try:
-                trip_date = datetime.datetime(
-                    2023, int(date_input_list[1]), int(date_input_list[0])
-                    )
-                rate_column = enginesize_ws.col_values(3)
-                rate_column.pop(0)
-                full_name_string = full_name[0]
-                destination_string = destination[0]
-                print("Please ensure this trip you entered is correct:")
-                print(
-                    f"\t{full_name_string} to {destination_string} on "
-                    f"{(trip_date.strftime('%a %d %b %Y'))}"
-                    )
-                # user_input = input("Press Y to proceed,
-                # or C to cancel & try again")
-                print("Press Y to proceed    or    C to cancel & try again")
-                position = name_column.index(full_name_string)
-                rate = rate_column[position]
-                position = destination_column.index(destination_string)
-                distance_column = distance_ws.col_values(2)
-                distance_column.pop(0)
-                print(distance_column)
-                distance = distance_column[position]
-                print(distance)
-                amount = round(float(rate) * int(distance))
-                print(amount)
-                print(
-                    datetime.datetime.now(), full_name_string,
-                    trip_date, amount
-                    )
-                travel_expenses_ws = SHEET.worksheet('TravelExpenses')
-                travel_expenses_ws.append_row(
-                    [(datetime.datetime.now()).strftime('%a %d %b %Y'),
-                     full_name_string, (trip_date.strftime('%a %d %b %Y')),
-                     amount]
-                    )
-                print("Updated worksheet")
-            # this except is only true is 1st try stmt fails
-            except ValueError as e:
-                print(
-                    f"Invalid date : {e} - {date_input} Enter valid dd/mm"
-                    )
-"""
