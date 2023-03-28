59f12eac84c96f6165babe36ab1c34c264dccd66
siobhain <sobrien@live.ie>
2023-03-28 13:29:05 +0000

    Correct indentation Remove whitespace Insert more comments Pass Linter

--- run.py
+++ run.py
@@ -17,12 +17,43 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('ccd-travel-expenses')
 
+"""
+The 3 worksheets in SHEET that are relevent to this app are
+    TravelExpenses, Distance & EngineSize
+Their FIELDS and sample data are outlined below and correct as of March 2023
+
+TravelExpenses : Log of 2023 travel expenses/trip records
+Fields & Example
+STATUS	 SUBMITDATE	EMPLOYEE	  DESTINATION	AMOUNT	TRAVELDATE	ID-NUM
+Approved 27 Mar 	Maura Murtagh Quarry	    17.36	Fri 20 Jan 	2023-21
+Pending	 27 Mar 	Tarah Roche	  Quarry	    8.36	Sun 12 Feb  2023-23
+
+NOTE : the ID_NUM field is auto generated when trip record added
+
+Distance : Round trip distance from office location to destination
+Fields & Example
+DESTINATION	    DISTANCE-IN-KM
+Browns	                55
+
+EngineSize : Car engine size of employees authorised to claim travel expenses
+EMPLOYEE	ENGINE-SIZE-cc	ALLOWANCE-PER-KM
+Anne Tully	1600	        51.82
+
+FYI
+The allowances per KM are as follows
+1200cc	41.80cent
+1600cc	51.82cent
+1400cc	43.40cent
+
+"""
+
 
 def get_main_menu():
     """
-    This function asks user to enter a number from 1 to 4
-    The input string is checked for validation in a 'try'
-    The user will be asked to try again until a valid number is entered
+    This function is called from main()
+    This function prints main menu to screen and calls prompt_user
+    prompt_user returns the menu choice of the user
+    the menu choice is returned to main()
 
     """
 
@@ -39,14 +70,20 @@ def get_main_menu():
 
 def prompt_user(num):
     """
-    This function
-    process users input from 1 to num
+    This function prompts user to Enter a number from 1 to num
+    where num is the size of the menu option being processed for instance
+    main menu has 4 options so user will be asked to Enter 1,2,3 or 4
+    whereas report menu has 3 options so user will be asked to Enter 1,2 or 3
 
-    removes spaces from input
-    check for number between 1 nad num passed into the function
-    caters for all occurances of invalid input from nothing inout
-
+    The user is prompted to enter a menu number within a while loop which will
+    continue until a valid numeric is entered.  This will end the loop and the
+    user menu number chosen is returned. Inside the while loop the user input
+    is handled in a try block which caters for :
+        blank input
+        non-numeric value input
+        numeric value input but out of range
     """
+# controls while loop -breaks once valid user_choice from 1 to 'num' entered
     user_choice = 0
     # returns list of consecutive numbers 1 to num-1
     num_list = [i for i in range(1, num)]
@@ -54,7 +91,7 @@ def prompt_user(num):
     while user_choice not in range(1, num+1):
         try:
             u_input = input(prompt)
-            # remove spaces if any
+            # remove spaces if any input
             user_input = u_input.replace(" ", "")
             if user_input == "":
                 raise ValueError(" Input is blank")
@@ -69,9 +106,27 @@ def prompt_user(num):
     return int(user_choice)
 
 
-def log_a_trip():
+def log_travel_expenses_trips():
     """
-    Asks user to log a trip
+    This function is run when user chose option #1 from main menu, It is the
+    main function for adding travel expense records to the database.
+
+    The user is presented with information on the format & order of data to
+    be input in order to add a travel expense record (trip) to the database &
+    informed how the app works, then user is asked to enter their first record.
+
+    Function  then enters a while loop which only breaks when user indicates
+    they do not have any more trip records to enter.
+
+    Once trip record data is input, it is validated by get_trip_data &
+    not returned until correct information input (user informed all the while),
+    Once return from get_trip_data we know we have correct data to construct a
+    travel expenses record and create_trip_record is fired which returns a
+    record, User is asked to verify this record is correct before submitting
+    to the database, so record is either submitted or discarded depending
+    User then asked if they have trip record to input and if not the while loop
+    is broken & user returned to main menu - otherwise loop run again to
+    enter another travel expense recorsd
     """
     print("\n CCD TRAVEL EXPENSE RECORDS 2023\n")
 
@@ -103,7 +158,7 @@ def log_a_trip():
 
     while True:
         trip_input = get_trip_data()
-        # data has been validated and verified at this stage
+        # data input has been validated and verified at this stage
         record = create_trip_record(trip_input)
         headings = return_header_row("TravelExpenses")
         view_record = dict(zip(headings, record))
@@ -124,7 +179,20 @@ def log_a_trip():
 
 def get_trip_data():
     """
-    Ask user to enter 3 items to record the trip
+    This function starts with a while loop that
+    waits for user to enter 3 items to record the travel expense (trip)
+    These items are NAME,DESTINATION, TRAVELDATE & only breaks out of the
+    loop once the input is validated & 'Trip details accepted', therefore
+    function only finishes on valid input from user & returns the data in
+    list format having removed the comma delimiter
+    The error exception handling is achieved in a 'try' block to check
+    the input in a series of elif's & raise an error if
+        input is blank
+        input is too short (need 3 items entered aka 2 commas input)
+        name or destination shorter than 3 letters
+
+    Returns a list when valid trip record is input by user
+
     """
     while True:
         user_input = input()
@@ -141,13 +209,6 @@ def get_trip_data():
             elif validate_trip_data(trip_input):
                 print("Trip details are accepted, Constructing the record...")
                 break
-# In theory the next else should not be entered but in the event there are
-# exception cases I have not thought about or catered for
-            # else:
-            #     print(
-            #         "Invalid Input Try Again \n ENTER  " +
-            #         "Employee Name, Destination, Date of Travel"
-            #         )
         except ValueError as e:
             print(f"Invalid Input: {e} Please try again\n")
             print("Enter Employee Name, Destination, Travel Date")
@@ -156,15 +217,27 @@ def get_trip_data():
 
 def validate_trip_data(trip_input):
     """
-    Three items input for trip
-    NAME - needs to match name in spreadsheet
-    DESTINATION - needs to match destination in spreadsheet
-    DATE - needs to be valid date
+    This functions is used to validate the travel expense input by the user
 
+    There are 3 items input, NAME, DESTINATION, TRAVELDATE
+        NAME & Destination that are input can be substring > 2 letters
+            NAME - needs to exist in the db (EngineSize ws gsheet)
+            DESTINATION - needs to exist in the db (Distance ws gsheet)
+        DATE - needs to be valid date
+    This function does the following
+
+    Divide the list of data into 3 string variables
+    Verify the name, the destination, the date in series of nestd if's
+    returning True if all If's passed
+    or
+    Returning False & Asking user to try again After appropriate message
+    displayed to user as to what the problem with their input
     """
+    # strip removes any leading/trailing whitespace
     name_input = trip_input[0].strip()
     destination_input = trip_input[1].strip()
     date_input = trip_input[2].strip()
+
     date_input_list = date_input.split("/")
     if verify_data(name_input, 'EngineSize'):
         if verify_data(destination_input, 'Distance'):
@@ -194,14 +267,14 @@ def validate_trip_data(trip_input):
 
 def verify_data(data_input, worksheet):
     """
-    This function check if data is in worksheet, used for employee name
-    and destination aa both in first column of worksheet
+    This function check if data entered by user exists in DB/worksheet
+    It is used to verify employee name and destination as both are in first
+    column of corresponding worksheet
     """
     data_ws = SHEET.worksheet(worksheet)
     data_column = data_ws.col_values(1)
     # 1st column in EngineSize worksheet contains employee names
     # 1st column in Distance worksheet contains destinations
-
     if data_input.lower() in str(data_column).lower():
         return True
     else:
@@ -210,32 +283,51 @@ def verify_data(data_input, worksheet):
 
 def create_trip_record(trip_input):
     """
-    Expand the data/
-    perform processing before entry to spreadsheet
-    record needs to be in following order
-    STATUS SUBMITDATE NAME DESTINATION AMOUNT TRAVELDATE
+    This function builds the Travel Expenses record (trip record)
+    based on the data input by the user in the following steps
+
+    1. The Employee Name and Destination details that were inut are
+    converted/expanded to full text using expand_data function, This is
+    required as user can input as little as 3 characters for name/destination
+
+    2. Date input in dd/mm is converted to database format ie Tue 23 May
+    There is no year saved in the date as database/gsheet is for 2023, There
+    will be a new gsheet for every year
+
+    3. Calculations are then carried out to determin the reimbursement amount
+    due to employee, This is the only figure that the trip record holds, the
+    distance and mileage rate (although km) applied are all done in the
+    background
+        Destination used to determine KM travelled in get_distance_in_km
+        Employee Name (trip_name) used to determine Rate in rate_in_cent_per_km
+    Both these figures used to calculate amount due to employee, Pleaae note
+    that Civil service mileage rates depends on car engine size.
+
+    All this information is used to construct the trip_record in List format
+    & this value is returned, All new trip_records have STATUS = Pending
+
     """
-    # get full name and destination strings
+
+    # expand substring to full name/destination strings
     trip_name = expand_data(trip_input[0].strip(), 'EngineSize')
     trip_destination = expand_data(trip_input[1].strip(), 'Distance')
 
-    # convert dd/mm that was input to format held in gsheet DB
+    # convert dd/mm that was input into database/gsheet format
     date_input_list = trip_input[2].strip()
-
     trip_date = date(
         2023, (int((date_input_list[1]))), (int((date_input_list[0])))
         ).strftime('%a %d %b %Y')
 
     # today is the submit date
     today = (date.today()).strftime('%a %d %b %Y')
-
-    # retrieve the milage allowance for the employee
+    # retrieve the mileage allowance for the named employee
     rate_in_cent_per_km = get_milage_rate(trip_name)
     # retrieve the distance for the trip
     distance = get_distance_in_km(trip_destination)
     # calculate in €'s the amount due to employee
     amount = round(((float(rate_in_cent_per_km) * int(distance))/100), 2)
-    # Create the record that will be send to TravelExpenses worksheet
+    # Create the record that will be send to DB/TravelExpenses worksheet
+    # Field names are STATUS SUBMITDATE EMPLOYEE DESTINATION AMOUNT TRAVELDATE
     trip_record = list((
         "Pending", today, trip_name[0], trip_destination[0], amount, trip_date
             ))
@@ -245,8 +337,8 @@ def create_trip_record(trip_input):
 def get_milage_rate(name):
     """
     This function returns the milage rate in cent per kilometer for the name
-    sent in the EngineSize worksheet has column headings for the employees that
-    can claim travel NAME, ENGINE SIZE cc, Allowance per Km
+    sent in, the EngineSize worksheet has column headings for the employees
+    that can claim travel NAME, ENGINE SIZE cc, Allowance per Km
     There is a row for each employee that can claim travel expanses
     Open woksheet, find index of employee to get rate for that employee
     """
@@ -261,8 +353,13 @@ def get_milage_rate(name):
 
 def get_distance_in_km(destination):
     """
-    This function uses the destination send to it to #
-    find the distance of said destination from spreadsheet
+    This function uses the destination send to it to find
+    the assosiated distance and return this figure
+    The informatio is held in a 2 column spreadsheet called Distance
+    Column heading are Destination and Distance, The function identifies
+    the row number of the destination value with the index() method
+    the row_number is then used to get the corresponding distance
+    and return that value
     """
     distance_ws = SHEET.worksheet('Distance')
     destination_column = distance_ws.col_values(1)
@@ -291,6 +388,8 @@ def expand_data(data_input, worksheet):
 def return_header_row(worksheet_name):
     """
     This function returns the values of the 1st row of the worksheet send in
+    The headinga are used in displaying information to the user in various
+    situations
     """
     the_ws = SHEET.worksheet(worksheet_name)
     headings = the_ws.row_values(1)
@@ -299,8 +398,8 @@ def return_header_row(worksheet_name):
 
 def submit_trip_record(trip_details):
     """
-    This function updates the TravelExpenses worksheet with trip
-    entered by the user
+    This function updates the TravelExpenses worksheet with trip record
+    entered by the user, Confirmation is display to user
     """
 
     travel_expenses_ws = SHEET.worksheet('TravelExpenses')
@@ -310,8 +409,13 @@ def submit_trip_record(trip_details):
 
 def run_report_menu():
     """
-    This function asked user what report they want to see
-
+    This function displays the report menu with 3 options
+    User is them prompted to choose an option, all error/exception handling
+    done in the prompt_user function, once report_menu_choice is validated
+    the function takes appropriate action in a while lopo until
+    user finally enters option #3 and is returned to main menu
+    There are 2 reports options available for user to choose depending on
+    the STATUS of the Travel Expense record - Pending or Approved
     """
     report_menu = """\nREPORT MENU
     1) Enter 1 to list PENDING  Travel Expense records
@@ -330,7 +434,6 @@ def run_report_menu():
             run_pending_report()
         print(report_menu)
         report_menu_choice = prompt_user(3)
-
     return
 
 
@@ -377,9 +480,9 @@ def run_pending_report():
 def run_approved_report():
     """
     This function list all records in TravelExpense worksheet with
-    Status=PENDING
+    Status=Approved
     """
-    # STATUS is in 1st Column & is either PENDING or APPROVED or DECLINE
+    # STATUS is in 1st Column & is either Pending or Approved
     status_column = return_nth_column("TravelExpenses", 1)
     number_approved = status_column.count("Approved")
     if number_approved > 0:
@@ -421,9 +524,16 @@ def return_nth_column(worksheet_name, column_number):
 
 def approve_pending_records():
     """
-    This function display all pending records to the user
-    and gives them the choice of approving all together or
-    one by one
+    This function is fired when user picks option #3 from the main menu
+    This function display all pending travel expense records to the user
+    and gives them the choice of approving all together or one by one
+    Records are approved by changing the STATUS field to Approved
+    using update_cell, The record is odentified by the uniqueID automatically
+    assigned by the database (gsheet =if(A6<>"",CONCATENATE("2023-",row()),""))
+    to column G on TravelExpenses worksheet
+
+    This function could be improve by creating sub function to carry out the
+    the 2 while loops here to 'update_cell' & calling from here
     """
 
     pending_records = run_pending_report()
@@ -440,7 +550,7 @@ def approve_pending_records():
                     int(row_number[5:]), 1,  "Approved"
                         )
                 i += 1
-            print(f"{number_pending} records approved")      
+            print(f"{number_pending} records approved")
         else:
             i = 0
             travel_expenses_ws = SHEET.worksheet("TravelExpenses")
@@ -455,7 +565,7 @@ def approve_pending_records():
                         # slice '2023-' from row_number
                         int(row_number[5:]), 1,  "Approved"
                         )
-                    print("Approved......next record")    
+                    print("Approved......next record")
                 else:
                     print("Not approved...next rec")
                 i += 1
@@ -464,7 +574,16 @@ def approve_pending_records():
 
 
 def main():
-    """ Main program loop
+    """
+    Main program loop
+    Print the main menu which gives user 4 options with number 4 being to exit
+    Act on the other 3 options by firing the follwoing functions
+    #1 - Add travel expense record to the database - log_travel_expenses_trips
+    #2 - Reports section - a sub-menu for user to choose which report to run
+    #3 - Approval section - this is where a manager would approve or not
+        each travel expense record before reimbursement can be made
+    Once work is complete on each option, the main menu is again displayed
+    until the user enters option 4 to exit the app
     """
 
     user_main_choice = get_main_menu()
@@ -476,7 +595,7 @@ def main():
         elif user_main_choice == 2:
             run_report_menu()
         elif user_main_choice == 1:
-            log_a_trip()
+            log_travel_expenses_trips()
         user_main_choice = get_main_menu()
     print(" Goodbye")
 
